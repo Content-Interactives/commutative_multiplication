12cd88fa004a45aa5135ef2bff5d3e554f705948
User <user@example.com>
2025-04-17 13:23:41 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommutativeMultiplication from './components/CommutativeMultiplication'
+
+function App() {
+  return (
+      <div className="App">
+        <CommutativeMultiplication />
+      </div>
+  );
+}
+
+
+export default App;
